aa9bd83c2f0e3b0b8d21381a780463d526d1d9a3
Dima Moment <dimamoment28@outlook.com>
2020-03-09 18:45:59 +0200

    Added basic Data access layer

---
+++ SoftBox.DAL/Configuration/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using SoftBox.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SoftBox.DAL.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Login).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.PasswordHash).IsRequired().HasMaxLength(500);
+            builder.HasOne(x => x.UserProfile).WithOne(x => x.User).HasForeignKey<UserProfile>(x => x.UserId);
+        }
+    }
+}
---
+++ SoftBox.DAL/Configuration/UserProfileConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using SoftBox.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SoftBox.DAL.Configuration
+{
+    public class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+    {
+        public void Configure(EntityTypeBuilder<UserProfile> builder)
+        {
+            builder.ToTable("UserProfiles");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(30);
+            builder.HasOne(x => x.User).WithOne(x => x.UserProfile).HasForeignKey<User>(i => i.UserProfileId);
+        }
+    }
+}
